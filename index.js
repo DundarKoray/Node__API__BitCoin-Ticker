deeaef4221e66425019def07527b8eab18158bd8
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-04 11:53:17 +0300

    did some changes in order to upload to heroku

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ app.post("/", function(req, res){
     })
 })
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000.")
 })
