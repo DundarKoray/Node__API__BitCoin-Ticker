122a65dac90a2528ea278175d02fa51484d6946b
Koray Dundar <koraydundar_40@hotmail.com>
2019-06-04 14:12:00 +0300

    applied fixes for heroku

--- index.js
+++ index.js
@@ -46,9 +46,7 @@ app.post("/", function(req, res){
     })
 })
 
-app.post("/failure", function(req, res){
-    res.redirect("/")
-})
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000.")
